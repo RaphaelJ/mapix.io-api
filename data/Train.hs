40848c5cc72a2326eae5ff865c200ce7313cbad5
Raphael Javaux <raphaeljavaux@gmail.com>
2013-12-26 22:03:04 +0100

    file listing

--- data/Train.hs
+++ data/Train.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE BangPatterns #-}
+
 import Control.Applicative
 import Control.Monad
-import System.Directory (doesDirectoryExist, getDirectoryContents)
+import System.Directory (
+      doesDirectoryExist, doesFileExist, getDirectoryContents
+    )
 import System.Environment (getArgs)
-import System.FilePath (FilePath, (</>))
+import System.FilePath (FilePath, (</>), takeFileName)
+import Vision.Histogram (ToHistogram (..))
 import qualified Vision.Histogram as H
+import Vision.Image (Image (..), RGBImage)
 import qualified Vision.Image as I
 
 main :: IO ()
@@ -11,17 +17,31 @@ main = do
     args <- getArgs
     case args of
         [dir] -> do
-            dirs <- catsDirs dir
-            mapM loadDir dirs
+            dirs <- listDirs dir
 
-            
+            imgs <- concat <$> mapM loadSet dirs
 
             print ds
         _     -> putStrLn "Usage : ./train <training dir>"
   where
-    -- Returns every dirs in the training directory.
-    catsDirs dir =
-        map (dir </>) . excludeHidden <$> getDirectoryContents dir >>=
-        filterM doesDirectoryExist
-
+    -- Returns the list of non-hidden entries of a directory with an absolute
+    -- path.
+    listContent dir = map (dir </>) . excludeHidden <$> getDirectoryContents dir
     excludeHidden = filter (((/=) '.') . head)
+
+    listDirs  dir = listContent dir >>= filterM doesDirectoryExist
+    listFiles dir = listContent dir >>= filterM doesFileExist
+
+    loadSet :: FilePath -> IO [(String, RGBImage)]
+    loadSet dir = do
+        let catName = takeFileName dir
+
+        files <- listFiles dir
+        forM files $ \file -> do
+            Right img <- I.load file Nothing
+            return (catName, I.convert img)
+
+:: Image i, ToHistogram (ImagePixel i)
+=> [(String, i)] -> (i -> Histogram sh a)
+-> (Histogram sh a -> Histogram sh a -> b)
+-> 
