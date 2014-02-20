9d7aa83770519a04cb2449807717fe8d0d4656a1
Raphael Javaux <raphaeljavaux@gmail.com>
2014-02-20 20:03:31 +0100

    image index

--- api/ImageIndex/Manage.hs
+++ api/ImageIndex/Manage.hs
@@ -10,40 +10,34 @@ import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 
 newIndex :: IO ImageIndex
-newIndex = do
-    lastCalled <- LastCalledQueue <$> newTVarIO Nothing <*> newTVarIO Nothing
-                                  <*> newTVarIO M.empty
-    ImageIndex <$> newTVarIO M.empty <*> pure lastCalled
+newIndex = ImageIndex <$> newTVarIO M.empty <*> newTVarIO Nothing
+                      <*> newTVarIO Nothing
 
 -- Users -----------------------------------------------------------------------
 
 -- | Searches for an existing user index entry by the user name and returns it.
 -- If a such entry doesn\'t exist, creates a new one.
 getUserIndex :: ImageIndex -> UserName -> UTCTime -> STM UserIndex
-getUserIndex ImageIndex {..} username currentTime = do
+getUserIndex ii@(ImageIndex {..}) username currentTime = do
     iiUserVal <- readTVar iiUsers
     case M.lookup username iiUserVal of
         Just userIdx -> return userIdx
         Nothing      -> do
-            -- Creates the user index.
-            rootTag <- Tag RootTag <$> newTVar M.empty <*> newTVar S.empty
-            userIdx <- UserIndex username <$> newTVar M.empty <*> pure rootTag
+            userIdx <- attachNode ii currentTime newUserIndex
             writeTVar iiUsers (M.insert username userIdx iiUserVal)
-
-            -- Inserts the new user index in the last call queue.
-            node <- attachNode iiLastCalled currentTime (newNode userIdx)
-            modifyTVar (lcMap iiLastCalled) (M.insert username node)
 
             return userIdx
 
-    newNode userIdx prev next =
-        LastCalledNode userIdx <$> newTVar currentTime <*> newTVar prev
-                               <*> newTVar next
+    newUserIndex prev next = do
+        rootTag <- Tag RootTag <$> newTVar M.empty <*> newTVar S.empty
+        UserIndex username <$> newTVar M.empty  <*> pure rootTag
+                           <*> pure currentTime <*> newTVar prev
+                           <*> newTVar next
 
 removeUserIndex :: ImageIndex -> UserIndex -> STM ()
-removeUserIndex ImageIndex {..} UserIndex {..} = do
+removeUserIndex ii@(ImageIndex {..}) ui@(UserIndex {..}) = do
     modifyTVar' iiUsers (M.delete uiName)
-    detatchNode (uiName  
+    detatchNode ii ui
 
 -- Tags ------------------------------------------------------------------------
 
@@ -54,7 +48,7 @@ getTag UserIndex {..} tagPath = do
     readTVar uiRootTag >>= dfs tagPath
   where
     dfs []       tag               = return tag
-    dfs (t : ts) parent@(Tag {..}) =
+    dfs (t : ts) parent@(Tag {..}) = do
         subTags <- readTVar tSubTags
         tag <- case M.lookup t subTags of
             Just tag -> return tag
@@ -88,8 +82,7 @@ removeTag ui@(UserIndex {..}) tag@(Tag (SubTag name parent) subTags imgs) = do
     (M.elems  <$> readTVar subTags) >>= unBindSubs
     (S.toList <$> readTVar imgs)    >>= unBindImages
   where
-    unBindParent Nothing       = return ()
-    unBindParent (Just Tag {..}) = do
+    unBindParent Tag {..} = do
         subTags' <- M.delete name <$> readTVar tSubTags
         imgs'    <- readTVar tImages
         writeTVar tSubTags subTags'
@@ -160,10 +153,10 @@ removeImage ui@(UserIndex {..}) img@(Image {..}) = do
 -- Last called queue -----------------------------------------------------------
 
 -- | Updates the last call time for the user index and updates the least
--- recently used queue. Does nothing if the user index doesn\'t exist.
+-- recently used queue.
 -- O(n).
-touchUserIndex :: LastCalledQueue -> UserName -> UTCTime -> STM ()
-touchUserIndex lc@(LastCalledQueue {..}) username currentTime = do
+touchUserIndex :: ImageIndex -> UserIndex -> UTCTime -> STM ()
+touchUserIndex ImageIndex {..} username currentTime = do
     mNode <- M.lookup username <$> readTVar (lcMap iiLastCalled)
     case mNode of
         Just node -> do
@@ -181,41 +174,40 @@ touchUserIndex lc@(LastCalledQueue {..}) username currentTime = do
 -- corresponding LRU queue position (determined by @currentTime@) in which the
 -- returned node will be inserted. Returns the inserted node.
 -- O(n).
-attachNode :: LastCalledQueue -> UTCTime ->
-           -> (Maybe LastCalledNode -> Maybe LastCalledNode
-               -> STM LastCalledNode)
-           -> STM LastCalledNode
-attachNode LastCalledQueue {..} currentTime nodeFct =
-    go Nothing lcFirst
+attachNode :: ImageIndex -> UTCTime ->
+           -> (Maybe UserIndex -> Maybe UserIndex -> STM UserIndex)
+           -> STM UserIndex
+attachNode ImageIndex {..} currentTime idxFct =
+    go Nothing iiLRCFirst
   where
     go mPrev nextRef = do
         mNext <- readTVar nextRef
         case mNext of
-            Just next | lcnTime next < currentTime -> go next (lcnNext next)
-                      | otherwise                  -> do
+            Just next | uiLRCTime next < currentTime -> go next (uiLRCNext next)
+                      | otherwise                    -> do
                 -- Inner/First node.
-                node <- nodeFct mPrev mNext
-                writeTVar nextRef        (Just node)
-                writeTVar (lcnPrev next) (Just node)
-                return node
+                userIdx <- idxFct mPrev mNext
+                writeTVar nextRef          (Just userIdx)
+                writeTVar (uiLRCPrev next) (Just node)
+                return userIdx
             Nothing -> do
                 -- Last node.
-                node <- nodeFct mPrev mNext
-                writeTVar nextRef               (Just node)
-                writeTVar (lcLast iiLastCalled) (Just node)
-                return node
+                userIdx <- idxFct mPrev mNext
+                writeTVar nextRef   (Just userIdx)
+                writeTVar iiLRCLast (Just userIdx)
+                return userIdx
 
 -- Removes the specified node from the LRU queue.
 -- O(1).
-detatchNode :: ImageIndex -> LastCalledNode -> STM ()
-detatchNode LastCalledQueue {..} node = do
-    prev <- readTVar (lcnPrev node)
-    next <- readTVar (lcnNext node)
+detatchNode :: ImageIndex -> UserIndex -> STM ()
+detatchNode ImageIndex {..} userIdx = do
+    prev <- readTVar (uiLRCPrev userIdx)
+    next <- readTVar (uiLRCNext userIdx)
 
     case prev of
-        Just node' -> writeTVar (lcnNext node') next
-        Nothing    -> writeTVar lcFirst         next
+        Just userIdx' -> writeTVar (uiLRCNext userIdx') next
+        Nothing       -> writeTVar iiLRCFirst           next
 
     case next of
-        Just node' -> writeTVar (lcnPrev node') prev
-        Nothing    -> writeTVar lcLast          prev
+        Just userIdx' -> writeTVar (uiLRCPrev userIdx') prev
+        Nothing       -> writeTVar iiLRCLast            prev
--- api/ImageIndex/Type.hs
+++ api/ImageIndex/Type.hs
@@ -4,8 +4,13 @@ import Data.Int
 import Data.Map.Strict (Map)
 
 data ImageIndex = ImageIndex {
-      iiUsers      :: !(TVar (Map UserName UserIndex))
-    , iiLastCalled :: !LastCalledQueue
+      iiUsers    :: !(TVar (Map UserName UserIndex))
+    -- Here we define a concurrent double linked list which implements a Least
+    -- Recent Call queue for user indexes. Indexes are kept sorted by descending
+    -- last usage order so we can quickly remove indexes which haven't been used
+    -- for a long time.
+    , iiLRCFirst :: !(TVar (Maybe UserIndex)) -- ^ Most recent call.
+    , iiLRCLast  :: !(TVar (Maybe UserIndex)) -- ^ Least recent call.
     }
 
 -- Each API user has its own index. Index are used to search for image by tag or
@@ -17,6 +22,9 @@ data UserIndex = UserIndex {
       uiName    :: !UserName
     , uiImages  :: !(TVar (Map Hmac Image))
     , uiRootTag :: !Tag
+    , uiLRCTime :: !UTCTime
+    , uiLRCPrev :: !(TVar (Maybe UserIndex)) -- ^ More recently called user.
+    , uiLRCNext :: !(TVar (Maybe UserIndex)) -- ^ Less recently called user.
     }
 
 -- Here we define two types of tags.
@@ -45,22 +53,4 @@ data Image = Image {
 data Histogram = Histogram {
     , h5D    :: !(H.Histogram DIM5 Float)
     , h3D    :: !(H.Histogram DIM3 Float)
-    }
-
--- Here we define a concurrent double linked list which implements a Least
--- Recently Used queue for user indexes. Indexes are kept sorted by descending
--- last usage order so we can quickly remove indexes which haven't been used for
--- a month.
-
-data LastCalledQueue = LastCalledQueue {
-      lcFirst :: !(TVar (Maybe LastCalledNode)) -- ^ Most recent call.
-    , lcLast  :: !(TVar (Maybe LastCalledNode)) -- ^ Least recent call.
-    , lcMap   :: !(TVar (Map UserName LastCalledNode))
-    }
-
-data LastCalledNode = LastCalledNode {
-      lcnUser :: !UserIndex
-    , lcnTime :: !(TVar UTCTime)
-    , lcnPrev :: !(TVar (Maybe LastCalledNode)) -- ^ More recent call.
-    , lcnNext :: !(TVar (Maybe LastCalledNode)) -- ^ Less recent call.
     }
