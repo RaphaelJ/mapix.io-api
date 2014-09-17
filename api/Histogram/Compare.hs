52bf3994c764fe9297f673bfb968530c67d9ebd2
Raphael Javaux <raphaeljavaux@gmail.com>
2014-09-17 22:24:46 +0200

    Neighborhood comparison of two 1D histograms using vectors instead of lists

--- api/Histogram/Compare.hs
+++ api/Histogram/Compare.hs
@@ -8,7 +8,7 @@ import Data.Function
 import qualified Data.Vector.Storable as V
 import Foreign.Storable (Storable)
 import Vision.Histogram (Histogram (..), compareIntersect, index, linearIndex)
-import Vision.Primitive (Z (..), (:.) (..), DIM1, ix1, shapeList)
+import Vision.Primitive (DIM1, ix1, shapeLength)
 
 import Histogram.Config (confPerfectFitBinWeight)
 import Histogram.Type (HeterogeneousHistogram (..), PartialHistogram (..))
@@ -36,18 +36,22 @@ comparePartial PartialHistogram {..} HeterogeneousHistogram {..} =
 
 compareHist1D :: (Storable a, Ord a, Fractional a)
             => Histogram DIM1 a -> Histogram DIM1 a -> a
-compareHist1D hist1@(Histogram sh1 _) hist2@(Histogram sh2 _)
+compareHist1D hist1@(Histogram sh1 vec1) hist2@(Histogram sh2 vec2)
     | sh1 /= sh2 = error "Histograms are not of equal size."
     | otherwise  =
-          compareIntersect hist1 hist2  * confPerfectFitBinWeight
-        + sum [ min v1 v2 | Z :. ix <- shapeList sh1
-              , let !v1 =   hist1 `index` ix1 (ix - 1)
-                          + hist1 `index` ix1 ix
-                          + hist1 `index` ix1 (ix + 1)
-              , let !v2 =   hist2 `index` ix1 (ix - 1)
-                          + hist2 `index` ix1 ix
-                          + hist2 `index` ix1 (ix + 1)
-              ]
-          * inv / 3
+          compareIntersect hist1 hist2      * confPerfectFitBinWeight
+        + V.sum (V.izipWith step vec1 vec2) * inv / 3
   where
     !inv = 1.0 - confPerfectFitBinWeight
+
+    !maxIx = shapeLength sh1 - 1
+
+    step ix v1 v2 =
+        let !leftIx  | ix == 0     = maxIx
+                     | otherwise   = ix - 1
+            !rightIx | ix == maxIx = 0
+                     | otherwise   = ix + 1
+        in min (hist1 `index` ix1 leftIx + v1 + hist1 `index` ix1 rightIx)
+               (hist2 `index` ix1 leftIx + v2 + hist2 `index` ix1 rightIx)
+{-# SPECIALIZE compareHist1D :: Histogram DIM1 Float -> Histogram DIM1 Float
+                             -> Float #-}
